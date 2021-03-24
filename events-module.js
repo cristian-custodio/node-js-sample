ecc4f442f46d07f53b001367517302eff9eb9158
Cristian Custodio <cristian325@gmail.com>
2021-03-23 23:25:36 -0400

    Entending a logger module

--- events-module.js
+++ events-module.js
@@ -3,9 +3,24 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 //Register a listener (of the Event)
-emitter.on("messageLogged", function () {
-  console.log("Listener called");
+emitter.on("messageLogged", function (arg) {
+  console.log("Listener called", arg);
 });
 
 //Raise and Event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+
+//Extending a Module
+
+//var url = "http://mylogger.io/log";
+
+// class Logger extends EventEmitter {
+//   log(message) {
+//     console.log(message);
+
+//     //Raise and Event
+//     this.emit("messageLogged", { id: 1, url: "http://" });
+//   }
+// }
+
+// module.exports = Logger;
